aea948d4f114ed6bc6bf98b40b1e84af6ab116a9
Nikhil-sharma04 <69202464+Nikhil-sharma04@users.noreply.github.com>
2021-06-28 18:57:39 +0530

    Add files via upload

---
+++ src/main/java/com/nagarro/training/EmployeeManagementApplication.java
@@ -0,0 +1,18 @@
+package com.nagarro.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author nikhilsharma05
+ *
+ */
+@SpringBootApplication
+public class EmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagarro/training/EmployeeManagementApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nagarro.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/**
+ * 
+ * @author nikhilsharma05
+ *
+ */
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
